a1d5574a41f355c49b947a861969185c08bbabde
Cody Spratford <koreanpanda345@gmail.com>
2024-02-27 20:15:25 -0800

    Update replays.ts

--- src/routes/replays.ts
+++ src/routes/replays.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import Replay from "../models/replay";
+import Replay, { IReplay } from "../models/replay";
 const router = Router();
 
 router.route('/').post(async (req, res) => {
@@ -19,7 +19,25 @@ router.route('/').post(async (req, res) => {
   await newReplay.save();
 });
 router.route('/:id').get(async(req, res)=> {
-  return res.send("Success");
+  const replay = await Replay.findOne<IReplay>({id: req.params.id}).exec();
+  if (!replay) {
+    res.status(404).send("Could not find that replay");
+    return;
+  }
+  let buf = '<!DOCTYPE html>\n';
+		buf += '<meta charset="utf-8" />\n';
+		buf += '<!-- version 1 -->\n';
+		buf += `<title>${replay.format} replay: ${replay.players[0]} vs. ${replay.players[1]}</title>\n`;
+		buf += '<div class="wrapper replay-wrapper" style="max-width:1180px;margin:0 auto">\n';
+		buf += '<div class="battle"></div><div class="battle-log"></div><div class="replay-controls"></div><div class="replay-controls-2"></div>\n';
+		buf += `<h1 style="font-weight:normal;text-align:center"><strong>${replay.format}</strong><br /><a href="https://pokemonshowdown.com/users/${replay.players[0]}" class="subtle" target="_blank">${replay.players[0]}</a> vs. <a href="https://pokemonshowdown.com/users/${replay.players[1]}" class="subtle" target="_blank">${replay.players[1]}</a></h1>\n`;
+		buf += '<script type="text/plain" class="battle-log-data">' + replay.log.replace(/\//g, '\\/') + '</script>\n';
+		buf += '</div>\n';
+		buf += '</div>\n';
+		buf += '<script>\n';
+		buf += `let daily = Math.floor(Date.now()/1000/60/60/24);document.write('<script src="https://play.thetrainercorner.net/js/replay-embed.js?version'+daily+'"></'+'script>');\n`;
+		buf += '</script>\n';
+    return res.status(200).send(buf);
 });
 
 export {
