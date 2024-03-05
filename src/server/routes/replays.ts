49b5b240b8c975dd38ed6635876920fcccc66252
Cody Spratford <koreanpanda345@gmail.com>
2024-03-04 17:38:26 -0800

    adding a index page

--- src/server/routes/replays.ts
+++ src/server/routes/replays.ts
@@ -3,6 +3,32 @@ import Replay, { IReplay } from "../models/replay";
 import { discordClient } from "../../global/constants";
 const router = Router();
 
+router.route("/").get(async (req, res) => {
+  let buf = "<!doctype html>";
+  buf += `<html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>TTC | Replay Home</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>`;
+  buf += `<body><ul class="nav justify-content-center"><li class="nav-item"><a class="nav-link active" aria-current="page" href="https://play.thetrainercorner.net">Main Server</a></li><li class="nav-item"><a class="nav-link" href="https://test.thetrainercorner.net">Test Server</a></li><li class="nav-item"><a class="nav-link" href="https://dex.thetrainercorner.net">Dex Site</a></li><li class="nav-item"><a class="nav-link disabled" aria-disabled="true">Replay Server</a></li></ul>`;
+  buf += `<div class="container text-center">\n`;
+  
+  const replays = await Replay.find();
+  let amount = replays.length >= 25 ? replays.length : 25
+  for (let i = 0; i < amount; i++) {
+    buf += `<div class="row">\n`;
+    const replay = replays.at(i);
+    buf += `<div class="card">\n`;
+    buf += `<div class="card-body">\n`;
+    buf += `<h5 class="card-title">${replay!.players[0]} VS ${replay!.players[1]}</h5>\n`;
+    buf += `<h6 class="card-subtitle mb-2 text-body-secondary">Format: ${replay!.format}</h6>\n`;
+    buf += `<a href="https://replay.thetrainercorner.net/${replay?.id}">Watch Replay</a>\n`;
+    buf += `</div>\n`;
+    buf += `</div>\n`;
+
+  }
+  buf += `</div>\n`;
+  buf += `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>\n`;
+  buf += `</body>\n`;
+  buf += `</html>\n`;
+});
+
 router.route("/").post(async (req, res) => {
   console.debug(req.headers.host);
   const newReplay = new Replay({
